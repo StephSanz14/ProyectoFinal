a1955477110d8b0d1f09961e568d6e41f9441961
Stephany <steph_sap@outlook.es>
2025-11-27 15:56:44 -0600

    update

--- 02.Project/ecommerce-api/src/config/database.js
+++ 02.Project/ecommerce-api/src/config/database.js
@@ -6,15 +6,10 @@ dotenv.config();
 const dbConnection = async () => {
   try {
     const dbURI = process.env.MONGODB_URI;
-    const dbName = process.env.MONGODB_DB;
 
-    await mongoose.connect(`${dbURI}/${dbName}`, {
-      // If you use MongoDB < 8 you have to use this:
-      //useNewUrlParser:true,
-      //useUnifiedTopology:true
-    });
+    await mongoose.connect(dbURI);
 
-    console.log(`MongoDB is connected`);
+    console.log('MongoDB is connected');
   } catch (error) {
     console.log(error);
     process.exit(1);
