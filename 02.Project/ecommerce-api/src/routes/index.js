fe4a40ebede2ba075f10afd37b1113b9ddc7a57e
Stephany <steph_sap@outlook.es>
2025-12-11 01:28:26 -0600

    wishbackendupdate

--- 02.Project/ecommerce-api/src/routes/index.js
+++ 02.Project/ecommerce-api/src/routes/index.js
@@ -5,10 +5,11 @@ import cartRoutes from './cartRoutes.js';
 import categoryRoutes from './categoryRoutes.js';
 import notificationRoutes from './notificationRoutes.js';
 import orderRoutes from './orderRoutes.js';
-import paymentMethodRoutes from './paymentMethodRoutes.js';
+import paymentMethodRoutes from './paymentMethodRoutes.js'; 
 import productRoutes from './productRoutes.js';
 import userRoutes from './userRoutes.js';
 import shippingAddressRoutes from './shippingAddressRoutes.js';
+import wishlistRoutes from './wishList.routes.js';
 
 const router = express.Router();
 
@@ -16,6 +17,7 @@ router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
 router.use('/shipping-address', shippingAddressRoutes);
 router.use(cartRoutes);
+router.use(wishlistRoutes);
 router.use(categoryRoutes);
 router.use(notificationRoutes);
 router.use(orderRoutes);
